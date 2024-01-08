b6d5e862789ee0a0a87df08464ab91c2fc9de4da
just juan <74630241+juangpartida@users.noreply.github.com>
2024-01-08 15:54:58 -0800

    Add files via upload
    Assignment #1: includes python code plus read me

---
+++ project-1-animal.py
@@ -0,0 +1,7 @@
+# Author: Juan Guillermo Partida
+# Date: 1/08/2024
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
